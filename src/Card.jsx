2c047aa3082315d9fdc09e2eeacac6c3c1b325f5
intellic <intellic@student.21-school.ru>
2025-02-13 23:06:29 +0500

    fix classname, add default props

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Card(card) {
+function Card({name="Unknown", status="Unknown", created="Unknown"}) {
     return (
     <>
     <div class="card">
         <div className="card_name-container">
-            <h1 class="card_name">
-                {card.name}
+            <h1 className="card_name">
+                {name}
             </h1>
         </div>
         <div className="card_bottom-container">
             <div className="card_status">
-                {card.status}
+                {status}
             </div>
             <div className="card_created">
-                {card.created}
+                {created}
             </div>
         </div>
-    </div>;
+    </div>
     </>
     )
 }
@@ -28,11 +28,5 @@ Card.propTypes = {
     status: PropTypes.string,
     created: PropTypes.string
 };
-
-Card.defaultProps = {
-    name: "Unknown",
-    status: "Unknown",
-    created: "Unknown"
-  };
 
 export default Card;
