6d71b37ad60036d3a3b9f03a25026ec17a47a17e
intellic <intellic@student.21-school.ru>
2025-02-13 23:06:09 +0500

    add test card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css'
+import Card from './Card.jsx'
+
+const testCard = {
+  name: "Rick",
+  status: "Alive",
+  created: "13.02.2025",
+  id: 1
+}
 
 function App() {
-  
+  return (
+    <>
+    <Card name={testCard.name} status={testCard.status} created= {testCard.created}/>
+    </>
+  )
 }
 
 export default App
