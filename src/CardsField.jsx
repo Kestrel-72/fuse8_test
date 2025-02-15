3beb2b3cdda326a7d2351dfa12e72e3d3fd2aa17
intellic <intellic@student.21-school.ru>
2025-02-15 16:21:09 +0500

    remove buttons from CardsField

--- src/CardsField.jsx
+++ src/CardsField.jsx
@@ -1,21 +1,10 @@
 import CardsList from "./CardsList.jsx";
 
 function CardsField( { apiResult } ) {
-    let buttons = [];
-
-    if (apiResult != null && apiResult.info.pages > 1) {
-        for (let i = 1; i <= apiResult.info.pages; i++) {
-            buttons.push(<input type="button" key={i} value={i}></input>);
-        }
-    }
-
     return (
-        <>
-            <div className="cardsField">
-                <CardsList apiResult={apiResult}/>
-            </div>
-            <ul>{buttons}</ul>
-        </>
+        <div className="cardsField">
+            <CardsList apiResult={apiResult}/>
+        </div>
     )
 }
 
